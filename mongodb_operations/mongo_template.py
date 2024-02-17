cfe8c485597ab21b36d80d769a28b69747d461b6
Binwen Wu <2390726421@qq.com>
2024-02-18 06:05:10 +0800

    feat: Add parsing and warehousing of PhysicalField data

--- mongodb_operations/mongo_template.py
+++ mongodb_operations/mongo_template.py
@@ -1,21 +1,3 @@
-"""
-该资产在MongoDB中的一条记录的例子, 其他类似:
-"id": "1",
-"spatialDimension": [106_130],
-"timeDimension": [20230106],
-"productDimension": "1",
-"featureDimension": "230000",
-"viewpointDimension": [1],
-"genericName": "3dsim_Building",
-"boundingVolume": {"type":"AABB", "bv": [-73.9612660018684,40.6707788088504,0,-73.7000626459572,40.7902961004823,205.564101845492]},
-"transformToWorld": [],
-"creationDate": "20230106",
-"validTimeSpan": ['19000101','29991212'],
-"adeOfMetadata": {"asset":{"version":"1.0","extras":{"ion":{"georeferenced":"true","movable":"true","renderable":"true","shadowCasting":"true","shadowReceiving":"true","transformable":"true","visible":"false"}}}},
-"renderStyle": ""
-"""
-
-
 template_scene_asset = {
     "_id": "", # identifier
     "spatialDimension": [],
@@ -127,7 +109,7 @@ template_model_asset_physicalfield = {
         "fileType": "" # the type of model file, i.e., file suffixe
     },
     "resolution": "", # the resolution of model
-    "pixelThinkness": "",
+    "pixelThinkness": "", # the pixel thinkness of model
     # and another attributes for the model, such as resolution, material, etc.
 }
 
